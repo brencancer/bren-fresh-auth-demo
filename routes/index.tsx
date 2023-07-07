a8b14c6cb4aaac177150d9881b7ff2051e094f30
frissons <dodongbren@gmail.com>
2023-07-07 18:48:38 +0800

    hope it works, it works on my machine

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,3 +1,4 @@
+// deno-lint-ignore-file no-explicit-any
 import Layout from "../components/Layout.tsx";
 import { Handlers, PageProps } from "$fresh/server.ts";
 import { State } from "./_middleware.ts";
